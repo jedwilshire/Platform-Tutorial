35e20a67afa8d2eaf815c448eac144fb38beb868
Jed Wilshire <jedwilshire@gmail.com>
2021-10-14 18:41:02 -0500

    Lesson 01 - Motion equations are used with vectors for position, velo…
    …city, and acceleration in combination with a friction constant to demonstrate movement of a sprite over a tile grid. Press the 'h' key to see the grid and statistics for the vectors and the frame rate. removed platform class

--- Lesson_01/sprites.py
+++ Lesson_01/sprites.py
@@ -42,15 +42,3 @@ class Player(pygame.sprite.Sprite):
             self.acc.y = -PLAYER_ACC
         if keys[pygame.K_DOWN]:
             self.acc.y = PLAYER_ACC
-
-class Platform(pygame.sprite.Sprite):
-    def __init__(self, game, x, y, width, height):
-        super().__init__()
-        self.game = game
-        self.game.platforms.add(self)
-        self.game.all_sprites.add(self)
-        self.image = pygame.Surface((width, height))
-        self.image.fill(GREEN)
-        self.rect = self.image.get_rect()
-        self.rect.x = x
-        self.rect.y = y
