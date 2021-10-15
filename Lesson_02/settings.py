f6b0505d1f88f176c6e8b0ef30ddc63677cb93e7
Jed Wilshire <jedwilshire@gmail.com>
2021-10-14 19:29:21 -0500

    Lesson 2 not complete

---
+++ Lesson_02/settings.py
@@ -0,0 +1,31 @@
+# Screen Settings
+TILE_SIZE = 32
+WIDTH = TILE_SIZE * 32
+HEIGHT = TILE_SIZE * 20
+WIDTH_TILES = 32
+HEIGHT_TILES = 20
+TITLE = 'Platform Tutorial'
+FPS = 60
+PLATFORM_LAYER = 1
+PLAYER_LAYER = 2
+
+# Physics Settings
+GRAVITY = 4
+FRICTION = -0.1 # A friction force
+ZERO_TOLERANCE = 0.2 # A value less than this is considered 0.0
+
+# Color Constants
+RED = (255, 0, 0)
+BLUE = (0, 0, 255)
+GREEN = (0, 255, 0)
+BLACK = (0, 0, 0)
+GREY = (20, 20, 20)
+YELLOW = (255, 255, 0)
+ORANGE = (255, 128, 0)
+WHITE = (255, 255, 255)
+
+# Player Settings
+PLAYER_WIDTH = TILE_SIZE
+PLAYER_HEIGHT = TILE_SIZE
+PLAYER_ACC = 10.0
+JUMP_ACC = 12.0
---
+++ Lesson_02/sprites.py
@@ -0,0 +1,67 @@
+import pygame
+from settings import *
+class Player(pygame.sprite.Sprite):
+    def __init__(self, game, x, y):
+        super().__init__()
+        self.game = game
+        self.game.all_sprites.add(self, layer = PLAYER_LAYER)
+        self.image = pygame.Surface((PLAYER_WIDTH, PLAYER_HEIGHT))
+        self.image.fill(ORANGE)
+        self.rect = self.image.get_rect()
+        self.rect.midbottom = (x, y)
+        self.pos = pygame.math.Vector2(x, y)
+        self.vel = pygame.math.Vector2(0, 0)
+        self.acc = pygame.math.Vector2(0, 0)
+        #self.jumping = False
+        
+    def update(self):
+        self.acc = pygame.math.Vector2(0, GRAVITY)
+        self.check_keys()
+        # apply friction to acceleration in x direction only
+        self.acc.x += self.vel.x * FRICTION
+        self.vel += self.acc
+        
+        # check platforms
+        self.check_for_platforms()
+        
+        # allow for stopping if vel is near zero in either direction
+        if abs(self.vel.x) < ZERO_TOLERANCE:
+            self.vel.x = 0
+        if abs(self.vel.y) < ZERO_TOLERANCE:
+            self.vel.y = 0
+    
+        # dp = 1/2 * a * dt^2 + v*dt
+        self.pos += 1/2 * self.acc * self.game.dt ** 2 + self.vel * self.game.dt
+        
+        # use bottom middle of sprite to position
+        self.rect.midbottom = self.pos
+    
+    def check_for_platforms(self):
+        self.rect.bottom += 2 # move down temporarily
+        for platform in self.game.platforms:
+            if platform.rect.collidepoint(self.rect.midbottom):
+                self.rect.bottom = platform.rect.top
+                self.vel.y = 0
+                return True
+        self.rect.bottom -= 2 # move back to position
+        return False
+    
+    def check_keys(self):
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_LEFT]:
+            self.acc.x = -PLAYER_ACC
+        if keys[pygame.K_RIGHT]:
+            self.acc.x = PLAYER_ACC
+ 
+
+class Platform(pygame.sprite.Sprite):
+    def __init__(self, game, x, y, width, height):
+        super().__init__()
+        self.game = game
+        self.game.platforms.add(self)
+        self.game.all_sprites.add(self, layer = PLATFORM_LAYER)
+        self.image = pygame.Surface((width * TILE_SIZE, height * TILE_SIZE))
+        self.image.fill(GREEN)
+        self.rect = self.image.get_rect()
+        self.rect.x = x * TILE_SIZE
+        self.rect.y = y * TILE_SIZE
